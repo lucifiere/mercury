e104829013c8638af6230c98810ea11518e3f21c
xd.wang <lucifiere@126.com>
2017-06-02 18:01:16 +0800

    增加filter

---
+++ src/main/java/com/atlandes/common/constant/HttpCode.java
@@ -0,0 +1,13 @@
+package com.atlandes.common.constant;
+
+/**
+ * Created by XD.Wang on 2017/6/2.
+ * HTTP状态码
+ */
+public interface HttpCode {
+
+    String RESOURCE_NOT_FOUND = "404";
+
+    String AUTHENTICATION_REFUSED = "403";
+
+}
